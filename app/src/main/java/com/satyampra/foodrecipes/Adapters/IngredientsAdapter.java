6a6aed4bd8f68de1986c86b70c481fe257197204
Satyam Prajapati <satyam7355pra@gmail.com>
2022-02-13 17:37:56 +0530

    5th commit details created

---
+++ app/src/main/java/com/satyampra/foodrecipes/Adapters/IngredientsAdapter.java
@@ -0,0 +1,58 @@
+package com.satyampra.foodrecipes.Adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.satyampra.foodrecipes.Model.ExtendedIngredient;
+import com.satyampra.foodrecipes.R;
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+public class IngredientsAdapter extends RecyclerView.Adapter<IngredientsViewHolder>{
+    Context context;
+    List<ExtendedIngredient> list;
+
+    public IngredientsAdapter(Context context, List<ExtendedIngredient> list) {
+        this.context = context;
+        this.list = list;
+    }
+
+    @NonNull
+    @Override
+    public IngredientsViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new IngredientsViewHolder(LayoutInflater.from(context).inflate(R.layout.list_meal_ingredients,parent,false));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull IngredientsViewHolder holder, int position) {
+        holder.textview_ingredients_name.setText(list.get(position).name);
+        holder.textview_ingredients_name.setSelected(true);
+        holder.textview_ingredients_quantity.setText(list.get(position).original);
+        holder.textview_ingredients_quantity.setSelected(true);
+        Picasso.get().load("https://spoonacular.com/cdn/ingredients_100x100/"+list.get(position).image).into(holder.imageView_ingredients);
+    }
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+}
+class IngredientsViewHolder extends RecyclerView.ViewHolder{
+    TextView textview_ingredients_quantity,textview_ingredients_name;
+    ImageView imageView_ingredients;
+
+    public IngredientsViewHolder(@NonNull View itemView) {
+        super(itemView);
+        textview_ingredients_quantity=itemView.findViewById(R.id.textview_ingredients_quantity);
+        textview_ingredients_name=itemView.findViewById(R.id.textview_ingredients_name);
+        imageView_ingredients=itemView.findViewById(R.id.imageView_ingredients);
+    }
+}
--- app/src/main/java/com/satyampra/foodrecipes/RecipeDetailsActivity.java
+++ app/src/main/java/com/satyampra/foodrecipes/RecipeDetailsActivity.java
@@ -1,17 +1,72 @@
 package com.satyampra.foodrecipes;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.Dialog;
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.satyampra.foodrecipes.Adapters.IngredientsAdapter;
+import com.satyampra.foodrecipes.Listeners.RecipeDetailsListener;
+import com.satyampra.foodrecipes.Model.RecipeDetailsResponse;
+import com.squareup.picasso.Picasso;
 
 public class RecipeDetailsActivity extends AppCompatActivity {
     int id;
+    TextView textView_meal_name,textView_meal_source,textView_meal_summery;
+    RecyclerView recycler_meal_Ingredients;
+    ImageView imageView_meal_image;
+    RequestManager manager;
+    ProgressDialog dialog;
+    IngredientsAdapter ingredientsAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipe_details);
-
+        
+        findViews();
         id=Integer.parseInt(getIntent().getStringExtra("id"));
+        manager=new RequestManager(this);
+        manager.getRecipeDetails(recipeDetailslistener,id);
+        dialog=new ProgressDialog(this);
+        dialog.setTitle("Loading Title");
+        dialog.show();
     }
+
+    private void findViews() {
+        textView_meal_name=findViewById(R.id.textView_meal_name);
+        textView_meal_source=findViewById(R.id.textView_meal_source);
+        textView_meal_summery=findViewById(R.id.textView_meal_summery);
+        recycler_meal_Ingredients=findViewById(R.id.recycler_meal_Ingredients);
+        imageView_meal_image=findViewById(R.id.imageView_meal_image);
+    }
+
+    private final RecipeDetailsListener recipeDetailslistener=new RecipeDetailsListener() {
+        @Override
+        public void didFetch(RecipeDetailsResponse response, String message) {
+            dialog.dismiss();
+            textView_meal_name.setText(response.title);
+            textView_meal_source.setText(response.sourceName);
+            textView_meal_summery.setText(response.summary);
+            Picasso.get().load(response.image).into(imageView_meal_image);
+
+            recycler_meal_Ingredients.setHasFixedSize(true);
+            recycler_meal_Ingredients.setLayoutManager(new LinearLayoutManager(RecipeDetailsActivity.this, LinearLayoutManager.HORIZONTAL,false));
+            ingredientsAdapter=new IngredientsAdapter(RecipeDetailsActivity.this,response.extendedIngredients);
+            recycler_meal_Ingredients.setAdapter(ingredientsAdapter);
+        }
+
+        @Override
+        public void didError(String message) {
+            Toast.makeText(RecipeDetailsActivity.this, message, Toast.LENGTH_SHORT).show();
+
+        }
+    };
 }
